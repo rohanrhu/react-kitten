ce5fd796563332fe1b7bccf759b425bc51e1b246
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2024-09-05 17:55:18 +0300

    Bump version 0.3.2

--- src/kitten.ts
+++ src/kitten.ts
@@ -10,7 +10,7 @@
  * (MIT License: https://opensource.org/licenses/MIT)
  */
 
-export const version = '0.3.1'
+export const version = '0.3.3'
 
 export function isMobileDevice() {
     return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
